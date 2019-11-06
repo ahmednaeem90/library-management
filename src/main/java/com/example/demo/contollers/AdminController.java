b663e1af805bd9847c08e0cb8645017f13b7bf32
Syed <P2906066@CORP.CHARTERCOM.COM>
2019-11-06 15:47:54 -0600

    added view, delete, add & login

--- src/main/java/com/example/demo/contollers/AdminController.java
+++ src/main/java/com/example/demo/contollers/AdminController.java
@@ -1,18 +1,40 @@
 package com.example.demo.contollers;
 
 import com.example.demo.domain.Librarian;
+import jdk.nashorn.internal.objects.annotations.Function;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdminController {
 
-    @GetMapping("/view")
-    public String viewLibrarian(){
+    @GetMapping("/add")
+    public String addLibrarian() {
         Librarian librarian = new Librarian("eddy", 25, 211);
-        return "hello view librarian ";
+        return "hello add librarian ";
         //return librarian;
     }
 
-    //Create remining endpoints
-}
+    @GetMapping("/view")
+    public String viewLibrarian() {
+        Librarian librarian = new Librarian("eddy", 25, 211);
+        return "hello view librarian";
+    }
+
+    @GetMapping("/delete")
+    public String deleteLibrarian() {
+        Librarian librarian = new Librarian("eddy", 25, 211);
+        return "hello delete librarian";
+
+        }
+
+
+        //Create remaining endpoints
+    @GetMapping("/logout librarian")
+    public String logoutLibrarian() {
+        Librarian librarian = new Librarian("eddy", 25, 211);
+        return "librarian logout";
+    }
+
+    }
